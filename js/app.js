a9358b62001f3415ec1a393dbdc9953bd61e8cfc
Kevin Cieszkowski <kevinjc@gmx.com>
2013-07-01 08:47:21 -0700

    Portfolio check-in

--- js/app.js
+++ js/app.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
 	$('.carousel').carousel({
 		interval:false
 	});
+
+	setHeight();
+	
+	$(window).resize(function(){
+		setHeight();
+	});
 });
+
+setHeight = function() {
+	$('.portfolio .carousel .item img').css('max-height','auto');
+	var height = $(window).height() - 130;
+	$('.portfolio .carousel .item img').css('max-height',height);
+}
